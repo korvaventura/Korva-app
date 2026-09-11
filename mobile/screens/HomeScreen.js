281301ee2dcacdb130baa25cbfb95b9378c8fa50
korvaventura <korvaventura@gmail.com>
2026-09-11 22:23:01 +0200

    fix: desactivar tutorial que crashea

--- mobile/screens/HomeScreen.js
+++ mobile/screens/HomeScreen.js
@@ -85,8 +85,7 @@ export default function HomeScreen({ navigation }) {
       if (session?.user?.id) {
         setUserId(session.user.id);
         try {
-          const tutorialVisto = await AsyncStorage.getItem('tutorial_visto');
-          if (!tutorialVisto) setMostrarTutorial(true);
+          await AsyncStorage.setItem('tutorial_visto', 'true');
         } catch (e) {}
         const metaNombre = session.user.user_metadata?.name?.split(' ')[0] || 
                            session.user.user_metadata?.full_name?.split(' ')[0] || '';
